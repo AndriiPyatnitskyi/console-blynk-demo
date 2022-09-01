529fac6d3517941098c878e09cc927c2235274ed
Andrii Piatnytskyi <andrii.piatnytskyi@tietoevry.com>
2022-09-01 21:51:20 +0300

    Remove init field data from Invertor. Create 2 Invertor objects.

--- src/Invertor.java
+++ src/Invertor.java
@@ -1,6 +1,6 @@
 public class Invertor {
-    float invPower = 29.9f;
-    float invI = 9.7f;
-    float invU = 625.0f;
-    boolean invActive;
+    float power;
+    float i;
+    float u;
+    boolean active;
 }
--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,39 @@
 public class Main {
-    // init data from invertor that we consume
-
-
     public static void main(String[] args) {
-        Invertor invertor = new Invertor();
+        Invertor artynsk = new Invertor();
+        artynsk.active = true;
+        artynsk.power = 30f;
+        artynsk.i = 5f;
+        artynsk.u = 10f;
 
-        switchInvertorStatus(true, invertor);
+        switchInvertorStatus(true, artynsk);
+        System.out.println("isActive: " + artynsk.active);
+        printInvertorData(artynsk);
 
-        System.out.println("isActive: " + invertor.invActive);
+        //
+        System.out.println("------------------");
+        //
 
-        printInvertorData(invertor);
+        Invertor zelena = new Invertor();
+        zelena.active = true;
+        zelena.power = 40f;
+        zelena.i = 10f;
+        zelena.u = 10f;
+
+        switchInvertorStatus(false, zelena);
+        System.out.println("isActive: " + zelena.active);
+        printInvertorData(zelena);
     }
 
     private static void printInvertorData(Invertor invertor) {
-        if (invertor.invActive) {
-            System.out.println("power: " + invertor.invPower);
-            System.out.println("i: " + invertor.invI);
-            System.out.println("u: " + invertor.invU);
+        if (invertor.active) {
+            System.out.println("power: " + invertor.power);
+            System.out.println("i: " + invertor.i);
+            System.out.println("u: " + invertor.u);
         }
     }
 
     public static void switchInvertorStatus(boolean switchStatus, Invertor invertor) {
-        invertor.invActive = switchStatus;
+        invertor.active = switchStatus;
     }
 }
